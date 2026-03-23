bdca8d8c56c9e0bf3765fb6833998d45f0ac7336
mattom <ygggrt45610@gmail.com>
2026-03-23 20:36:30 +0900

    modified sort program (translate to upper case before sorting)

--- src/bin/past_1st_F.rs
+++ src/bin/past_1st_F.rs
@@ -1,8 +1,7 @@
+use std::collections::HashMap;
 use std::io::stdin;
 extern crate regex;
 use regex::Regex;
-
-// ソート処理の実装が必要
 
 fn split_phrase(buf: &String) -> Vec<&str> {
     let re = Regex::new(r"[A-Z][a-z]*[A-Z]").unwrap();
@@ -18,11 +17,20 @@ fn main() {
     let mut buf: String = String::new();
     stdin().read_line(&mut buf).expect("failed");
 
-    let mut phrases = split_phrase(&buf);
+    let phrases = split_phrase(&buf);
 
-    phrases.sort(); // wrong
+    let mut upper_phrases: Vec<_> = vec![];
+    for str in phrases.clone() {
+        upper_phrases.push(str.to_ascii_uppercase());
+    }
+    upper_phrases.sort();
 
+    let mut to_upper_map: HashMap<_, _> = HashMap::new();
     for str in phrases {
-        print!("{}", str);
+        to_upper_map.insert(str.to_ascii_uppercase(), str);
+    }
+
+    for str in upper_phrases {
+        print!("{}", to_upper_map[&str]);
     }
 }
