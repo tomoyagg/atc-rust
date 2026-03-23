4e495cf69781ad08eaad5df0c7e3939f96fc04bc
mattom <ygggrt45610@gmail.com>
2026-03-23 19:55:19 +0900

    implement function split_phrase()

--- src/bin/past_1st_F.rs
+++ src/bin/past_1st_F.rs
@@ -4,18 +4,25 @@ use regex::Regex;
 
 // ソート処理の実装が必要
 
-fn main() {
-    let mut buf: String = String::new();
-    stdin().read_line(&mut buf).expect("failed");
-
+fn split_phrase(buf: &String) -> Vec<&str> {
     let re = Regex::new(r"[A-Z][a-z]*[A-Z]").unwrap();
 
     let mut v = vec![];
     for caps in re.captures_iter(&buf) {
         v.push(caps.get(0).unwrap().as_str());
     }
-    v.sort(); // wrong
-    for str in v {
-        print!("{}",str);
+    return v;
+}
+
+fn main() {
+    let mut buf: String = String::new();
+    stdin().read_line(&mut buf).expect("failed");
+
+    let mut phrases = split_phrase(&buf);
+
+    phrases.sort(); // wrong
+
+    for str in phrases {
+        print!("{}", str);
     }
 }
